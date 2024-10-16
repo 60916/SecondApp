0dfdc0c9e638e02de89c50880702388179e4bec1
DESKTOP-RQQ5IUJ\SpikenMC <60916@gymnasium.ax>
2024-10-16 13:47:00 +0300

    Jag har fixat lite med användarhantering

--- shoppinglist/Listor/views.py
+++ shoppinglist/Listor/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from typing import Any
+from django.db.models.query import QuerySet
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
 from .models import List, Item
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .forms import UserRegistrationForm
+from django.contrib import messages
 
 # Create your views here.
 class AllaListor (LoginRequiredMixin,ListView):
@@ -55,6 +62,18 @@ class UppdateraLista(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
       return True
     return False
 
+
+class RaderaLista(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+  model = List
+  success_url = '/'
+
+  def test_func(self):
+    lista=self.get_object()
+    if self.request.user == lista.list_user:
+      return True
+    return False
+
+
 class NyVara(LoginRequiredMixin,CreateView):
   model = Item
   fields = ['item_name', 'amount']
@@ -63,3 +82,31 @@ class NyVara(LoginRequiredMixin,CreateView):
     form.instance.list=get_object_or_404(List,id=self.kwargs.get('pk'))
     return super().form_valid(form)
 
+class UppdateraVara(LoginRequiredMixin,UpdateView):
+  model = Item
+  fields = ['item_name', 'amount']
+
+  def get_context_data(self,**kwargs):
+    context=super().get_context_data(**kwargs)
+    context['status'] = {'status':'Uppdatera'}
+    return context
+  
+class RaderaVara(LoginRequiredMixin,DeleteView):
+  model = Item
+
+  def get_success_url(self):
+    lista = self.object.list
+    return reverse_lazy('lista-sida', kwargs={'pk':lista.id})
+
+def registrera(request):
+  if request.method == 'POST':
+    form=UserRegistrationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      anvandarnamn = form.cleaned_data.get('username')
+      messages.success(request,f'Konto skapades för {anvandarnamn}')
+      return redirect('loggain')
+  else:
+    form=UserRegistrationForm()
+
+  return render(request, 'listor/registrera.html', {'form':form})
